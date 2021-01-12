a5474d67471b724be33576e327f75bf09ae70601
rodrigoespiga <rodrigoespiga@hotmail.com>
2021-01-12 11:03:15 +0100

    Creado componente chachi, 3 files modified, 2 added

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hola {{name}}!</h1><h5>otraCosa = {{otraCosa}}</h5>`,
+  template: `<h1>Hola {{name}}!</h1><h5>otraCosa = {{otraCosa}}</h5><app-mi-componente-chachi></app-mi-componente-chachi>`,
   styles: [`h1 { font-family: Lato; }`]
 })
 export class HelloComponent  {
---
+++ src/app/mi-componente-chachi/mi-componente-chachi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mi-componente-chachi',
+  templateUrl: './mi-componente-chachi.component.html',
+  styleUrls: ['./mi-componente-chachi.component.css']
+})
+export class MiComponenteChachiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
