3bb262670fd617ff82679c551db29088191a1758
rodrigoespiga <rodrigoespiga@hotmail.com>
2021-01-21 17:13:06 +0100

    Estilos, mostrar filas, columnas y loguear componentes, 6 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;  
-  public valorLeido:number;
-  public tamanoFuente:number = 12; //valor incialmente pasado al sizer
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  public valorLeido: number;
+  public tamanoFuente: number = 12; //valor incialmente pasado al sizer
 
-  public cambiarValorLeido(eventoRecibido){
+  ngOnInit() {
+    Array.from(document.querySelectorAll("*")).forEach(x => {
+      if (x.nodeName.indexOf("APP") != -1) {
+        console.log(x.nodeName);
+      }
+    });
+  }
+
+  public cambiarValorLeido(eventoRecibido) {
     this.valorLeido = eventoRecibido;
-
+    
   }
 }
