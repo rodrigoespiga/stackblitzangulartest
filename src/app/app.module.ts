6cdb10908b5e0ac761df332e377b6a2c124760ed
rodrigoespiga <rodrigoespiga@hotmail.com>
2021-01-21 13:00:40 +0100

    Añadido otro-componente, 4 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MiComponenteChachiComponent } from './mi-componente-chachi/mi-componente-chachi.component';
+import { OtroComponenteComponent } from './otro-componente/otro-componente.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MiComponenteChachiComponent ],
+  declarations: [ AppComponent, HelloComponent, MiComponenteChachiComponent, OtroComponenteComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/mi-componente-chachi/mi-componente-chachi.component.ts
+++ src/app/mi-componente-chachi/mi-componente-chachi.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-mi-componente-chachi',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mi-componente-chachi.component.css']
 })
 export class MiComponenteChachiComponent implements OnInit {
+  @Input() renderizarSubcomponente: boolean = false;
 
   constructor() { }
 
