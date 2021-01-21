1155cb646ec8fc84000f449e73810a55999256e1
rodrigoespiga <rodrigoespiga@hotmail.com>
2021-01-21 16:16:29 +0100

    Añadido Bootstrap 4.0, 7 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,11 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'hello',
-  template: `<h1>Hola {{name}}!</h1><h5>otraCosa = {{otraCosa}}</h5><app-mi-componente-chachi></app-mi-componente-chachi>`,
-  styles: [`h1 { font-family: Lato; }`]
+  selector: "hello",
+  template: `
+    <h3 class="alert alert-success">Hola {{ name }}!</h3>
+    <h5>otraCosa = {{ otraCosa }}</h5>
+    <app-mi-componente-chachi></app-mi-componente-chachi>
+  `,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `
+  ]
 })
-export class HelloComponent  {
+export class HelloComponent {
   @Input() name: string;
-  otraCosa = 'El valor de otra cosa';
+  otraCosa = "El valor de otra cosa";
 }
